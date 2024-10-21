0ff663fca35cd0b07d6a3add3a9263459bf7bce4
Iamkprasad <90253346+Iamkprasad@users.noreply.github.com>
2024-10-22 00:59:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     setTimeout(function() {
         document.getElementById('moon').style.opacity = 1;
         setTimeout(function() {
-            document.getElementById('poem').innerHTML = "You write a poem on cat<br>A feline friend, in night's soft hue<br>With eyes that glow, and whiskers so fine<br>A silent dance, in moon's soft light.";
+            document.getElementById('poem').innerHTML = "Under the full moon's radiant glow,<br>Your soul was born, a light to show,<br>Like silver beams in endless skies,<br>You shine with grace that never dies.";
             document.getElementById('poem').style.opacity = 1;
             setTimeout(function() {
                 document.getElementById('nextButton').style.display = 'block';
